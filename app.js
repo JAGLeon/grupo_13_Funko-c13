7ae9af16348cbc2c11e1f54325e2e9beaf0e3bb4
Leon <anthonygarayleon@gmail.com>
2022-03-07 19:05:57 -0300

    implementacion de express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const path  = require('path');
+const PORT = 3000;
 
+app.use(express.static(path.join(__dirname,'public')));
+
+// ROUTES
+
+// app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))}); creo que debe de ser así
+app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/login.html'))});
+app.get('/register',(req,res)=>{res.sendFile(path.join(__dirname,'./views/register.html'))});
+
+app.listen(PORT,()=> console.log(`Puerto ${PORT} 
+link: http://localhost:${PORT}`))
