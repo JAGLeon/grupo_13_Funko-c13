b365ec3c0c51a618306586d82345565a51b5e2d7
Camila Gaytan <camilagaytan05@gmail.com>
2022-04-13 23:58:46 -0300

    Arreglo de adminHeader

--- public/js/adminHeader.js
+++ public/js/adminHeader.js
@@ -1,3 +1,15 @@
+let menuButton = document.querySelector('.header-btnBars'),
+    menu = document.querySelector('.header-navMenu'),
+    menuCloseButton = document.querySelector('.header-menuClose');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.add('active')
+})
+
+menuCloseButton.addEventListener('click', () => {
+    menu.classList.remove('active')
+})
+
 /*adminUser*/
 let btnUser = document.querySelector('.header-btnMenu');
 let menuUser = document.querySelector('.header-ulUser');
