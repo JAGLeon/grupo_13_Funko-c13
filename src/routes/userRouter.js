baaba8f5e4e6ab51e7d87763847f961f40cf1f9b
Brian <rodriguezrbrian@gmail.com>
2022-07-02 11:27:43 -0300

    Añade profile validator

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const uploadFile = require('../middlewares/multer/upRegisterImg');
 const validateRegister = require('../validations/registerValidator');
 const validateLogin = require('../validations/loginValidator');
+const validateProfile = require('../validations/profileValidator')
 const userInSession = require('../middlewares/user/userInSession');
 const userSession = require('../middlewares/user/userSession');
 
@@ -12,7 +13,7 @@ router.post('/inicio', validateLogin, userController.loginUser);
 router.get('/registro', userInSession, userController.register);
 router.post('/registro', uploadFile.single('icon'), validateRegister, userController.registerUser);
 router.get('/perfil', userSession , userController.perfil);
-router.put('/perfil', uploadFile.single('icon'), userController.profileUpdate);
+router.put('/perfil', uploadFile.single('icon'), userSession , validateProfile ,userController.profileUpdate);
 router.post('/direcciones', userSession, userController.addressCreate);
 router.delete('/direcciones/:id', userSession, userController.addressDestroy);
 router.get('/salir', userController.logout);
---
+++ src/validations/profileValidator.js
@@ -0,0 +1,35 @@
+const { check, body } = require('express-validator');
+
+let validateProfile = [
+    check('userName')
+        .notEmpty().withMessage('Ingrese su usuario').bail()
+        .isLength({min:2, max:20}).withMessage('Ingrese un usuario valido'),
+    check('name')
+        .notEmpty().withMessage('Ingrese su nombre').bail()
+        .isAlpha().withMessage('Nombre inválido')
+        .isLength({min:2, max:30}).withMessage('Ingrese un nombre valido'),
+    check('lastName')
+        .notEmpty().withMessage('Ingrese su apellido').bail()
+        .isAlpha().withMessage('Apellido inválido')
+        .isLength({min:2, max:30}).withMessage('Ingrese un apellido valido'),
+    check("province")
+        .notEmpty().withMessage('Seleccione una provincia'),    
+    check("phone")
+        .notEmpty().withMessage('Ingrese su teléfono').bail()
+        .isAlpha().withMessage('Teléfono inválido')
+        .isLength({min:2, max:30}).withMessage('Ingrese un teléfono valido'),
+    body('icon')
+        .custom((value , {req}) => {
+            if(!req.file){
+                return true
+            } else if (req.file.mimetype === "image/png" || req.file.mimetype === "image/jpeg" || req.file.mimetype === "image/jpg"){
+                return true
+            } else {
+                return false
+            }
+        }).withMessage('Archivo de imagen en formato (.jpg - .jpeg - .png)')    
+    
+]
+    
+
+module.exports = validateProfile;
