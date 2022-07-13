af85605dbce592e2acb25d62799ea59f1063932b
Leon <anthonygarayleon@gmail.com>
2022-07-12 23:45:47 -0300

    dark mode

---
+++ public/js/users/darkMode.js
@@ -0,0 +1,14 @@
+let preferedColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+let $slider = document.getElementById('slider');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+$slider.addEventListener("click", () => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+})
+
+setTheme(localStorage.getItem('theme') || preferedColor);
