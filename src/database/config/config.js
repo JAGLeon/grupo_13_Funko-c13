d17988b37fe5f5ae46e5318ee7cdd2b3d22030b7
JAGLeon <anthonygarayleon@gmail.com>
2022-07-14 14:53:31 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "00419707",
+    "password": "",
     "database": "funko",
     "host": "127.0.0.1",
     "dialect": "mysql"
