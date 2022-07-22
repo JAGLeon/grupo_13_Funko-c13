0ec1cd68cac8a35315fc090bfa7585ff81f0a6f2
JAGLeon <anthonygarayleon@gmail.com>
2022-07-22 13:17:47 -0300

    slider automatic and dotenv

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const path  = require('path');
-const PORT =  3000;
+const PORT =  Number(process.env.PORT);
 const methodOverride = require('method-override');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('./middlewares/cookies/cookieSession');
 const db = require("./database/models");
-require("dotenv").config({path:".env"});
+require("dotenv").config();
+console.log(process.env);
 
 // Enrutadores 
 const productosRouter = require('./routes/productosRouter');
